2736c670f7c92e365b1a849435c9625092d83e7b
Ismi Ibragimov <ibragimovismi2022@gmail.com>
2025-05-29 15:44:08 +0300

    Message processing

--- server.py
+++ server.py
@@ -1,8 +1,17 @@
 from telegram.ext import Application, MessageHandler, filters
 from config import Token_for_Bot
 
+async def func(update, context):
+    print(update.message.chat.first_name)
+
+
 def main():
     aplication = Application.builder().token(Token_for_Bot).build()
+    text_handler = MessageHandler(filters.TEXT, func)
+    
+    aplication.add_handler(text_handler)
+    
+    aplication.run_polling()
     
     
 if __name__ == "__main__":
